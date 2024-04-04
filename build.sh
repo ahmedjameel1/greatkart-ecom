5f1f93b70b92516c2485af91dd11ab2ace1dbb08
AhmedJameel <roarme247@gmail.com>
2024-04-04 22:47:16 +0200

    update

--- build.sh
+++ build.sh
@@ -8,5 +8,11 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
+python manage.py makemigrations accounts
+python manage.py makemigrations category
+python manage.py makemigrations store
+python manage.py makemigrations carts
+python manage.py makemigrations orders
+
 # Apply any outstanding database migrations
 python manage.py migrate

57028ea8ef8bbef7f98b7d5ac62682746b6ba9ff
AhmedJameel <roarme247@gmail.com>
2024-04-05 01:29:23 +0200

    up

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
-python manage.py migrate
+python manage.py migrate --fake admin zero
 
 # python manage.py makemigrations accounts
 # python manage.py migrate accounts
@@ -20,3 +20,5 @@ python manage.py migrate
 # python manage.py migrate carts
 # python manage.py makemigrations orders
 # python manage.py migrate orders
+
+python manage.py migrate

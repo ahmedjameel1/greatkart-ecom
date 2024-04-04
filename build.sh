85d895e3c696c43a142cea962d1c041e4798c5d4
AhmedJameel <roarme247@gmail.com>
2024-04-04 22:54:29 +0200

    update

--- build.sh
+++ build.sh
@@ -13,10 +13,16 @@ python manage.py migrate --fake accounts zero
 
 
 python manage.py makemigrations accounts
+python manage.py migrate accounts
 python manage.py makemigrations category
+python manage.py migrate category
 python manage.py makemigrations store
+python manage.py migrate store
 python manage.py makemigrations carts
+python manage.py migrate carts
 python manage.py makemigrations orders
+python manage.py migrate orders
+
 
 # Apply any outstanding database migrations
 python manage.py migrate
